fa4fa65bd0daf15e6ea091777665948282b55ceb
schun92 <sarahsandiferchun@gmail.com>
2018-06-26 12:19:20 -0700

    used api for todo list

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React, { Component } from 'react';
 import AddItem from './add_item';
 import List from './list';
-import listData from '../data/list';
+import axios from 'axios';
 
 class App extends Component {
     constructor(props){
@@ -11,24 +11,53 @@ class App extends Component {
         this.state = {
             list: []
         };
+
+        this.base_url = 'http://api.reactprototypes.com';
+        this.api_key = '?key=c418demouser';
     }
 
     componentDidMount(){
         this.getListData();
     }
 
-    addItem(item){
-        this.setState({
-            list: [item, ...this.state.list]
-        });
+
+    async addItem(item){
+        try{
+            await axios.post(`${this.basu_url}/todos${this.api_key}}`, item);
+
+            this.getListData();
+        }catch(err){
+            console.log('Error adding item:', err.message);
+        }
     }
 
-    getListData(){
-        // Make call to server to get data
+    async getListData(){
+        
+        try {
+            const resp = await axios.get(`${this.base_url}/todos${this.api_key}`);
 
-        this.setState({
-            list: listData
-        });
+            this.setState({
+                list: resp.data.todos
+            });
+        }catch(err){
+            console.log('get Data Error:', err.response.data.error);
+        }
+
+
+        //using async and await makes code look like its working in a line
+
+        // Make call to server to get data
+        // axios.get(`${this.base_url}/todos${this.api_key}`).then(resp => {
+        //     console.log('Get todos Response:', resp.data.todos);
+
+        //     this.setState({
+        //         list: resp.data.todos
+        //     });
+
+        // }).catch(err => {
+        //     console.log('Get todos error:', err.message);
+        // })
+
     }
 
     render(){
